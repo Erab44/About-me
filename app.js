f40446b90c5f6bf1cce1902e60b6aecf43e3e514
Jeff McCord <jeff.mccord44@gmail.com>
2017-05-25 19:56:59 -0700

    Added second question in app.js

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 'use strict';
 
+var user = prompt('What is your name?');
+  console.log('The user\'s name is ' + user);
+
 var answer1 = prompt('This is a Yes or No answer, please reply with a yes or no. Does Jeff partake in alcohol?');
 if (answer1.toLowerCase() === 'yes') {
   alert('Incorrect! Jeff does not partake.');
 } else {
   alert('Correct!');
+}
+
+var answer2 = prompt('In regards to the first question, would he make his own Whiskey?');
+if (answer1.toLowerCase() === 'yes') {
+  alert('Good job. He is currently gathering materials to start distilling soon.')
+} else {
+  alert('Negative. He is planning to. Soon.');
 }
