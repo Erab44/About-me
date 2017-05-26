374baf6d37fb678e1b85881d7d359711e3200f52
Jeff McCord <jeff.mccord44@gmail.com>
2017-05-25 20:21:19 -0700

    Added 3rd question in app.js. fixed issue in question 2

--- app.js
+++ app.js
@@ -11,8 +11,16 @@ if (answer1.toLowerCase() === 'yes') {
 }
 
 var answer2 = prompt('In regards to the first question, would he make his own Whiskey?');
-if (answer1.toLowerCase() === 'yes') {
-  alert('Good job. He is currently gathering materials to start distilling soon.')
+if (answer2.toLowerCase() === 'yes') {
+  alert('Good job. He is currently gathering materials to start distilling soon.');
 } else {
   alert('Negative. He is planning to. Soon.');
+}
+
+var answer3 = prompt('And since he does not drink, he will need volunteers to occasionally test the taste and quality. Are you a willing volunteer for tasting?')
+if (answer3.toLowerCase() === 'yes') {
+  alert('Excellent! You\'ll be put on the list');
+  console.log(user + ' ' + answer3);
+} else {
+  alert('No worries. Thank you tho!');
 }
