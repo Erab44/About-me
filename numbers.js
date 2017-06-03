9aff592d365644a425c5356f3aae904c3c6f837a
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-06-03 13:48:46 -0700

    number game works

--- numbers.js
+++ numbers.js
@@ -1,18 +1,27 @@
-'use strict'
+'use strict';
 
 var number = Math.floor((Math.random() * 50) + 1);
+console.log(number);
 var turns = 4;
-var answer = prompt('Guess a number between 1 and 50');
+var answer = parseInt(prompt('Guess a number between 1 and 50'));
 
-
-while (turns > 0) {
-  if (answer > number) {
+while (turns >= 0) {
+  if(turns === 0){
+    alert('Sorry you ran out of turns');
+    break;
+  } else if (answer > number) {
     turns--;
-    document.write('Sorry, too high! Try again.');
+    //document.write('Sorry, too high! Try again.');
+    var answer = parseInt(prompt('Your number was too high. Try again.'));
+    continue;
   } else if (answer < number) {
     turns--;
-    document.write('So close, yet so far.');
+    //document.write('So close, yet so far.');
+    var answer = parseInt(prompt('Your number was too low. Try again'));
+    continue;
   } else if (number === answer) {
-    document.write('Correct! You guessed the secret number! ' + number);
+    alert('Congratulations! You guessed the secret number');
+    //document.write('Correct! You guessed the secret number! ' + number);
+    break;
   }
 }
