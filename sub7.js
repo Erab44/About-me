6560169b953c4787d55a9202c3b96284bbcd5c23
Jeff <jeff.mccord44@gmail.com>
2017-06-03 15:19:42 -0700

    changed name to be less confusing. added a while statment. changed perameters

---
+++ sub7.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var cities = ['redmond', 'kirkland', 'bellevue', 'glendale', 'north hollywood'];
+var guess = prompt('Which cities have I lived in across two states? You have 6 tries. Good luck!');
+var rounds = 6;
+
+while(rounds >= 0) {
+  for(var i = 0; i < cities.length; i++){
+    if(guess.toLowerCase() === cities[i]){
+      alert('Correct! I did live there!');
+      break;
+    } else {
+      rounds--;
+      alert('Negative. Try again. Only a few more options.');
+      continue;
+    }
+  }
+}
