289d0353615b374061f3e8b936ddd0a279b783ae
Jeff McCord <jeff.mccord44@gmail.com>
2017-06-06 21:24:07 -0700

    changed for loop to something new, no crashing

--- sub7.js
+++ sub7.js
@@ -4,13 +4,14 @@ var cities = ['redmond', 'kirkland', 'bellevue', 'glendale', 'north hollywood'];
 var guess = prompt('Which cities have I lived in across two states? You have 6 tries. Good luck!').toLowerCase;
 var rounds = 6;
 
-while(rounds >= 0) {
-  for(var i = 0; i < cities.length; i++){
-    if (guess === cities) {
-      alert ('Correct! I did live there!');
-    } else {
-      alert ('Negative, try a different one.')
-      rounds--;
-    }
-}
+for (i = 0; i < cities.length; i++){
+  var i = cities;
+  if (guess == cities[i]) {
+    alert ('Correct! I did live there!');
+    break;
+  } else {
+    alert ('Negative. Try something different.');
+    rounds--;
+    continue;
+  }
 }
