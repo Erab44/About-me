462a9febab11728a9a4695f9cc731d062e85ba32
Jeff McCord <jeff.mccord44@gmail.com>
2017-06-07 10:16:17 -0700

    no idea what I'm doing. can't seem to get it to work

--- sub7.js
+++ sub7.js
@@ -4,9 +4,8 @@ var cities = ['redmond', 'kirkland', 'bellevue', 'glendale', 'north hollywood'];
 var guess = prompt('Which cities have I lived in across two states? You have 6 tries. Good luck!').toLowerCase;
 var rounds = 6;
 
-for (i = 0; i < cities.length; i++){
-  var i = cities;
-  if (guess == cities[i]) {
+for (i = 0; i < cities.length; i++) {
+  if (guess == cities.length) {
     alert ('Correct! I did live there!');
     break;
   } else {
