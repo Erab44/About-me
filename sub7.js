e64d056fbe2dedf7e6e5849d7a69652c327a43c5
Jeff McCord <jeff.mccord44@gmail.com>
2017-06-06 20:54:28 -0700

    changed everything in while funct

--- sub7.js
+++ sub7.js
@@ -1,22 +1,16 @@
 'use strict';
 
 var cities = ['redmond', 'kirkland', 'bellevue', 'glendale', 'north hollywood'];
-var guess = prompt('Which cities have I lived in across two states? You have 6 tries. Good luck!');
+var guess = prompt('Which cities have I lived in across two states? You have 6 tries. Good luck!').toLowerCase;
 var rounds = 6;
 
 while(rounds >= 0) {
   for(var i = 0; i < cities.length; i++){
-    if (rounds === 0) {
-      alert('Sorry, you ran out of turns');
-      break;
-    } else if (guess.toLowerCase() === cities.indexOf[i]) {
-      rounds--;
-      console.log([i]);
-      alert('Correct! I did live there!');
+    if (guess === cities) {
+      alert ('Correct! I did live there!');
     } else {
+      alert ('Negative, try a different one.')
       rounds--;
-      alert('Negative. Try again. Only a few more options.');
-      continue;
     }
-  }
+}
 }
